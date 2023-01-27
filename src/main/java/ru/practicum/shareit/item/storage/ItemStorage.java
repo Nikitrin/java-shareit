6189cf1f5bc0c7e93f343031896b46b63503be7e
Nikitrin <nikita.trinoga@yandex.ru>
2023-01-28 00:41:36 +0300

    feat: new branch add-bookings

--- src/main/java/ru/practicum/shareit/item/storage/ItemStorage.java
+++ src/main/java/ru/practicum/shareit/item/storage/ItemStorage.java
@@ -1,10 +1,11 @@
 package ru.practicum.shareit.item.storage;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
 
-public interface ItemStorage {
+public interface ItemStorage extends JpaRepository<Item, Long> {
 
     Item createItem(Item item);
 
